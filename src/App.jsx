2c62fa36f01e7f8fb45e9c7a897e70aabb58ffa8
livanjr <livanjr@planprophet.com>
2023-11-06 20:01:28 -0500

    landing_page_livan_react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="body">
+      <Header />
+      <Card />
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export function Header() {
+  return (
+    <>
+      <div className="header">
+        <span className="header-title">Company</span>
+        <br />
+        <span className="header-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iusto
+          facilis vitae eveniet eos excepturi unde temporibus alias fugit! Ut
+          delectus eaque eveniet earum tenetur officia fugiat? Quo, quam
+          nostrum.
+        </span>
+      </div>
+    </>
+  );
+}
+
+export const Card = (props) => {
+  return (
+    <div>
+      <div className="small-div">
+        <i></i>
+        <img
+          src="C:\Users\livan\OneDrive\Documents\coding course\React project1\src\logo.jpg"
+          alt=""
+        />
+      </div>
+      <div className="section, bg-grey">
+        <span className="div-title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+          possimus sint neque laudantium vitae delectus aspernatur blanditiis
+          sequi rem accusamus quos necessitatibus velit ullam sunt quas quod.
+          Et, accusantium dolorem.
+        </span>
+      </div>
+      <div className="section">
+        <span className="div-title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+          possimus sint neque laudantium vitae delectus aspernatur blanditiis
+          sequi rem accusamus quos necessitatibus velit ullam sunt quas quod.
+          Et, accusantium dolorem.
+        </span>
+      </div>
+      <div className="section, bg-grey">
+        <span className="div-title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+          possimus sint neque laudantium vitae delectus aspernatur blanditiis
+          sequi rem accusamus quos necessitatibus velit ullam sunt quas quod.
+          Et, accusantium dolorem.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export function Footer() {
+  return (
+    <>
+      <div className="section">
+        <span className="footer">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iusto
+          facilis vitae eveniet eos excepturi unde temporibus alias fugit! Ut
+          delectus eaque eveniet earum tenetur officia fugiat? Quo, quam
+          nostrum.
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
